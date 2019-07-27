78811a5ce9974ea22edbae02ec5a15f7bacedb56
megyerimarton <megyerimarton@gmail.com>
2019-07-27 17:40:12 +0200

    Refactor class based App component to functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux'
 
 import store from './store';
 import Header from './components/Header';
 import List from './components/List';
 
-class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <div className="App">
-          <Header />
-          <List />
-        </div>
-      </Provider>
-    );
-  }
-}
+const App = () => (
+  <Provider store={store}>
+    <div className="App">
+      <Header />
+      <List />
+    </div>
+  </Provider>
+);
 
 export default App;
